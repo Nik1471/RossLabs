36c2e2400e81598797e9a326ce19cb7d4c762090
Nik1471 <Nik1471@users.noreply.github.com>
2021-03-05 22:12:54 +0300

    Add audio and database

--- StreamlabsManualAlerts.user.js
+++ StreamlabsManualAlerts.user.js
@@ -7,10 +7,20 @@
 // @icon         https://streamlabs.com/images/favicons/favicon.svg
 // @match        https://streamlabs.com/widgets/frame/alertbox/*
 // @require      https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js
-// @grant        none
+// @grant        GM_getValue
+// @grant        GM_setValue
 // ==/UserScript==
 
 /* global jQuery, $ */
+
+var alertUrl = 'https://cdn.twitchalerts.com/twitch-bits/sounds/bits.ogg'
+var alertAudio = new Audio(alertUrl);
+
+var database = GM_getValue('database');
+if (!database) {
+    database = [];
+    GM_setValue('database', database);
+}
 
 this.$ = jQuery.noConflict(true);
 
@@ -23,7 +33,18 @@ $(function() {
                 var attr = $target.prop(mutation.attributeName);
                 if (attr === 'widget-AlertBox') {
                     console.log('Arrived! ' + attr);
-                    console.log($target);
+
+                    var message = JSON.stringify($target.find('#wrap').html());
+                    var time = new Date().getTime();
+
+                    database.push({
+                        "t": time,
+                        "m": message,
+                        "s": 0
+                    });
+
+                    GM_setValue('database', database);
+                    //alertAudio.play();
                 }
             }
         });
