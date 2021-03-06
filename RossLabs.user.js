b34a8ac2d774563e3d9a865460114a197bf422dd
Nik1471 <Nik1471@users.noreply.github.com>
2021-03-06 22:49:55 +0300

    Remove old messages, part 1 - modal styles, add library

--- RossLabs.user.js
+++ RossLabs.user.js
@@ -10,12 +10,13 @@
 // @require      https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js
 // @require      https://cdnjs.cloudflare.com/ajax/libs/lodash.js/4.17.21/lodash.min.js
 // @require      https://cdnjs.cloudflare.com/ajax/libs/timeago.js/4.0.2/timeago.min.js
+// @require      https://cdnjs.cloudflare.com/ajax/libs/micromodal/0.4.6/micromodal.min.js
 // @grant        GM_addStyle
 // @grant        GM_getValue
 // @grant        GM_setValue
 // ==/UserScript==
 
-/* global jQuery, $, _, timeago */
+/* global jQuery, $, _, timeago, MicroModal */
 
 var alertUrl = 'https://cdn.twitchalerts.com/twitch-bits/sounds/bits.ogg'
 
@@ -43,19 +44,33 @@ $(function() {
             '#alert-btn { width: 100%; height: 8vw; }',
             '.wrap-button > .my-button { position: absolute; top: 1.6vw; left: -5.5vw; }',
 
-            '.alert-counter { width: 100%; padding: 0.6vw; text-align: center; background-color: rgb(239 239 239 / 20%); border: 1px solid #767676; box-sizing: border-box; margin-bottom: 2vw; font-size: 2.4vw; color: #9e9e9e }',
+            '.alert-counter { width: 100%; padding: 0.6vw; text-align: center; background-color: rgb(239 239 239 / 20%); border: 1px solid #767676; box-sizing: border-box; margin-bottom: 2vw; font-size: 2.4vw; color: #9e9e9e; position: relative; }',
             '#message-curr, #message-all { color: white; opacity: 0.8; }',
             '#message-all { font-weight: 600; opacity: 1; }',
+            '.alert-counter > .my-button { position: absolute; top: 0.1vw; left: -5.5vw; font-size: 3.7vw; }',
 
-            '.my-button { font-size: 4.5vw; line-height: 1; color: white; background-color: transparent; border: 0; padding: 0; opacity: 0; z-index: 10; }',
+            '.my-button { font-size: 4.5vw; line-height: 1; color: white; background-color: transparent; border: 0; padding: 0; opacity: 0; z-index: 10; transition: opacity 0.2s; }',
             '.my-button:hover { opacity: 0.9; }',
             '.my-button:active { opacity: 0.6; }',
             '.my-button:focus { outline: 0; }',
 
-            '.alert-nav { width: 100%; margin-bottom: 1vw; }',
+            '.alert-nav { width: 100%; margin-bottom: 0.5vw; }',
             '.alert-nav > .my-button { width: 50%; }',
 
-            '.time { margin-left: 0.5em; font-size: 80%; opacity: 0.8; }'
+            '.time { margin-left: 0.5em; font-size: 80%; opacity: 0.8; }',
+
+            '.modal { display: none; position: fixed; z-index: 10000; }',
+            '.modal.is-open { display: block; }',
+            '.modal-overlay { position: fixed; top: 0; left: 0; right: 0; bottom: 0; background: rgba(0,0,0,0.3); display: flex; justify-content: center; align-items: center; }',
+            '.modal-container { background-color: #fff; padding: 1.1vw; max-width: 30vw; max-height: 100vh; border-radius: 1vw; overflow-y: auto; box-sizing: border-box; }',
+            '.modal-header { display: flex; justify-content: space-between; align-items: center; }',
+            '.modal-title { margin-top: 0; margin-bottom: 0; font-weight: bold; line-height: 1.25; color: #00449e; box-sizing: border-box; font-size: 1.7vw; }',
+            '.modal-close { background: transparent; border: 0; font-size: 1.7vw; margin-left: 1vw; }',
+            '.modal-header .modal-close::before { content: "✕"; font-weight: bold; }',
+            '.modal-header .modal-close:hover, .modal-btn:hover { opacity: 0.7; }',
+            '.modal-footer { margin-top: 1vw; }',
+            '.modal-btn { margin-right: 1vw; padding: 0.5vw 1vw; background-color: rgba(255,0,0,0.1); border: 1px solid #767676; }',
+            '.btn-primary { background-color: rgba(255,0,0,0.3); }'
         ].join('\n');
         GM_addStyle(cssCode);
 
@@ -67,11 +82,14 @@ $(function() {
 
         var blockHtml = '<div class="wrap-block">' +
             '<div class="alert-nav"><button id="prev-button" class="my-button">🡄</button><button id="next-button" class="my-button">🡆</button></div>' +
-            '<div class="alert-counter"><span id="message-curr">0</span> / <span id="message-all">0</span></div>' +
+            '<div class="alert-counter"><span id="message-curr">0</span> / <span id="message-all">0</span><button id="erase-button" class="my-button" data-micromodal-trigger="modal-1">⌦</div>' +
             '<div class="wrap-button"><button id="alert-btn">Next</button><button id="toggle-button" class="my-button">👁</button></div>' +
             '</div>';
+
+        var modalHtml = '<div id="modal-1" class="modal" aria-hidden="true"><div tabindex="-1" class="modal-overlay" data-micromodal-close><div class="modal-container" role="dialog" aria-modal="true" aria-labelledby="modal-1-title"><header class="modal-header"><h2 id="modal-1-title" class="modal-title">Remove saved messages</h2><button class="modal-close" aria-label="Close" data-micromodal-close></button></header><footer class="modal-footer"><button class="modal-btn">Except today</button><button class="modal-btn btn-primary">All messages</button></footer></div></div></div>';
+
         var $widget = $('#widget');
-        $widget.after(blockHtml);
+        $widget.after(blockHtml).before(modalHtml);
         $('#wrap').after('<div id="wrap-copy">');
         var $wrapCopy = $('#wrap-copy');
 
@@ -171,5 +189,7 @@ $(function() {
         $('#toggle-button').click(function() {
             $wrapCopy.find('#alert-text-wrap').toggle();
         });
+
+        MicroModal.init();
     }
 });
